0ecb497f164cd94a073378a4657ade12383efba8
Ian <barrowian1991@gmail.com>
2020-11-02 16:04:10 +0000

    small fix for adding vertices, not a great solution

--- Voxelcpp/Renderer/RenderPolygon.cpp
+++ Voxelcpp/Renderer/RenderPolygon.cpp
@@ -8,9 +8,18 @@ void RenderPolygon::CreatePolygon(){
 
     glBindVertexArray(VAO);
 
+    glm::vec3 vertexPoints[36];
+    int count = 0;
+    for (auto &&i : indices)
+    {
+        vertexPoints[count] = cubeVertices[i];
+        count++;
+    }
+    
+
     //copy vertices into array buffer for OpenGL
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec3) * sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);    
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPoints), vertexPoints, GL_STATIC_DRAW);    
 
     //set vertex attribute pointers
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), nullptr);
