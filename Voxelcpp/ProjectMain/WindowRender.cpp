7bf481a37a072afccafaa96da995ff3163364acf
Ian <barrowian1991@gmail.com>
2020-11-03 17:51:15 +0000

    Created camera class, small bug with some cubes moving position as camera moves

--- Voxelcpp/ProjectMain/WindowRender.cpp
+++ Voxelcpp/ProjectMain/WindowRender.cpp
@@ -3,7 +3,15 @@
 using namespace ProjectMain;
 using namespace renderer;
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height); 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xOffset, double yOffset);
+
+
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = 800.0f / 2.0f;
+float lastY = 600.0f / 2.0f;
+bool firstMouse = true;
 
 void WindowRender::Display(){
     //initialise glfw with OpenGL version 3 and core profile
@@ -13,7 +21,6 @@ void WindowRender::Display(){
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     std::unique_ptr<GLFWwindow, decltype(&glfwDestroyWindow)> window = std::unique_ptr<GLFWwindow, decltype(&glfwDestroyWindow)>(glfwCreateWindow(_width, _height, "Voxel", nullptr, nullptr), glfwDestroyWindow);
-    //GLFWwindow* window = glfwCreateWindow(_width, _height, "Voxel", NULL, NULL);
 
     if(window == NULL){
         std::cout << "Failed to creat GLFW Window" << std::endl;
@@ -23,22 +30,47 @@ void WindowRender::Display(){
     //Make window the main context on current thread
     glfwMakeContextCurrent(window.get());
 
+    glfwSetInputMode(window.get(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
         std::cout << "Failed to initialize GLAD" << std::endl;
         return;
     }
+
     //set viewport size
-    glViewport(0, 0, 800, 600);
+    glViewport(0, 0, _width, _height);
     //check for viewport resizing
     glfwSetFramebufferSizeCallback(window.get(), framebuffer_size_callback);
+    glfwSetCursorPosCallback(window.get(), mouse_callback);
+    glfwSetScrollCallback(window.get(), scroll_callback);
+
     RenderPolygon polygon;
     polygon.CreatePolygon();
+    origin = glm::translate(origin, glm::vec3(0.0f, 0.0f, 0.0f));
     model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-    projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+    view = camera.GetViewMatrix();
+    projection = glm::perspective(glm::radians(45.0f), _width / _height, 0.1f, 100.0f);
     glEnable(GL_DEPTH_TEST);
+
+    glm::vec3 cubePositions[] = {
+    glm::vec3( 0.0f,  0.0f,  0.0f), 
+    glm::vec3( 2.0f,  5.0f, -15.0f), 
+    glm::vec3(-1.5f, -2.2f, -2.5f),  
+    glm::vec3(-3.8f, -2.0f, -12.3f),  
+    glm::vec3( 2.4f, -0.4f, -3.5f),  
+    glm::vec3(-1.7f,  3.0f, -7.5f),  
+    glm::vec3( 1.3f, -2.0f, -2.5f),  
+    glm::vec3( 1.5f,  2.0f, -2.5f), 
+    glm::vec3( 1.5f,  0.2f, -1.5f), 
+    glm::vec3(-1.3f,  1.0f, -1.5f)  
+    };
+
     //wait for user to close window
     while(!glfwWindowShouldClose(window.get())){
+        //per-frame time logic
+        currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
         //Check for input
         processInput(window.get());
 
@@ -50,13 +82,21 @@ void WindowRender::Display(){
         polygon.shaderProgram.use();
 
         //create transformations
-        polygon.shaderProgram.SetMat4("model", model);
+        view = camera.GetViewMatrix();
+        //polygon.shaderProgram.SetMat4("model", model);
         polygon.shaderProgram.SetMat4("view", view);
         polygon.shaderProgram.SetMat4("projection", projection);
 
         //render container
         glBindVertexArray(polygon.VAO);
-        glDrawElements(GL_TRIANGLES, sizeof(polygon.indices), GL_UNSIGNED_INT, 0);
+        for (unsigned int i = 0; i < sizeof(cubePositions); i++)
+        {
+            model = glm::translate(origin, cubePositions[i]);
+            polygon.shaderProgram.SetMat4("model", model);
+            glDrawElements(GL_TRIANGLES, sizeof(polygon.indices), GL_UNSIGNED_INT, 0);
+        }
+        
+        //glDrawElements(GL_TRIANGLES, sizeof(polygon.indices), GL_UNSIGNED_INT, 0);
 
         //swap colour buffers
         glfwSwapBuffers(window.get());
@@ -78,31 +118,52 @@ void WindowRender::processInput(GLFWwindow* window){
         glfwSetWindowShouldClose(window, true);
     }
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS){
-        model = glm::translate(model, glm::vec3(0.0f, 0.01f, 0.0f));
+        camera.ProcessKeyboard(FORWARD, deltaTime);
     }
     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS){
-        model = glm::translate(model, glm::vec3(0.0f, -0.01f, 0.0f));
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
     }
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS){
-        model = glm::translate(model, glm::vec3(0.01f, 0.0f, 0.0f));
+        camera.ProcessKeyboard(RIGHT, deltaTime);
     }
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS){
-        model = glm::translate(model, glm::vec3(-0.01f, 0.0f, 0.0f));
+       camera.ProcessKeyboard(LEFT, deltaTime);
     }
     if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS){
-        model = glm::rotate_slow(model, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 0.01f));
+        view = glm::rotate_slow(view, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 0.01f));
     }
     if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS){
-        model = glm::rotate_slow(model, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, -0.01f));
+        view = glm::rotate_slow(view, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, -0.01f));
     }
     if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS){
-        model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.01f));
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, 0.01f));
         // scale += 0.001f;
         // transform = glm::scale(transform, glm::vec3(scale, scale, scale));
     }
     if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS){
-        model = glm::translate(model, glm::vec3(0.0f, 0.0f, -0.01f));
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -0.01f));
         // scale -= 0.01f;
         // transform = glm::scale_slow(transform, glm::vec3(scale, scale, scale));
     }
 }
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos){
+    if (firstMouse){
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xOffset = xpos - lastX;
+    float yOffset = lastY - ypos;
+
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xOffset, yOffset, true);
+}
+
+void scroll_callback(GLFWwindow* window, double xOffset, double yOffset){
+    camera.ProcessMouseScroll(yOffset);
+}
+
--- Voxelcpp/ProjectMain/WindowRender.h
+++ Voxelcpp/ProjectMain/WindowRender.h
@@ -1,15 +1,22 @@
 #include "MainHeader.h"
+#include "Camera.h"
 #include <iostream>
 
 namespace ProjectMain {
     class WindowRender{
         public:
-            const int _width = 800;
-            const int _height = 600;
+            const float _width = 800.0f;
+            const float _height = 600.0f;
+            glm::mat4 origin = glm::mat4(1.0f);
             glm::mat4 model = glm::mat4(1.0f);
             glm::mat4 view = glm::mat4(1.0f);
             glm::mat4 projection = glm::mat4(1.0f);
             float scale = 1.0f;
+
+            //timing
+            float deltaTime = 0.0f;
+            float lastFrame = 0.0f;
+            float currentFrame = 0.0f;
             
             void processInput(GLFWwindow* window);
             void Display();
